a9680b95166bbccdf8a0ce85097f950b758894f1
gabrielTotarelliamado <70151557+gabrielTotarelliamado@users.noreply.github.com>
2024-03-11 18:10:54 -0300

    Update exc03.java

--- exc03.java
+++ exc03.java
@@ -1,6 +1,9 @@
 package view;
 import controller.exc03con;
 public class exc03 {
-exc03con exc03 = new exc03con();
-int[] c =exc03con.bubble();
+	public static void main(String[] args) {
+		exc03con exc03 = new exc03con();
+		exc03con.bubble();
+	}
+
 }
